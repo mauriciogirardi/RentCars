c5d515167a8ec2e650a624ef59865fab4a221496
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-06-07 22:25:30 -0300

    Add import csv category

---
+++ api/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,49 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+export class ImportCategoryUseCase {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  public loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IImportCategory[] = [];
+      const stream = fs.createReadStream(file.path);
+
+      const parseFile = parse({});
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => resolve(categories))
+        .on("error", (err) => reject(err));
+    });
+  }
+
+  public async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.map(async (category) => {
+      const { description, name } = category;
+
+      const existCategory = this.categoriesRepository.findByName(name);
+
+      if (!existCategory) {
+        this.categoriesRepository.create({ description, name });
+      }
+    });
+  }
+}
