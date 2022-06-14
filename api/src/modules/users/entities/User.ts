ca3365ef51e22e8b67b295e70ca670461c94d166
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-06-14 18:10:21 -0300

    created entities user

---
+++ api/src/modules/users/entities/User.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column("varchar", { length: 30 })
+  name: string;
+
+  @Column("varchar", { length: 30, unique: true })
+  username: string;
+
+  @Column("varchar", { length: 50, unique: true })
+  email: string;
+
+  @Column("varchar", { length: 20 })
+  password: string;
+
+  @Column("varchar", { length: 10 })
+  driver_license: string;
+
+  @Column("boolean", { default: false })
+  isAdmin: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
