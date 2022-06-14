89af9ec72f92d78a29cee109704732355258299b
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-06-13 21:11:24 -0300

    Add entities Category and Specification

--- api/src/database/index.ts
+++ api/src/database/index.ts
@@ -1,8 +1,14 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
+import { Category } from "../modules/cars/entities/Category";
+import { Specification } from "../modules/cars/entities/Specification";
+
 // docker-compose exec app node --require ts-node/register ./node_modules/typeorm/cli.js migration:run -d src/database
-// yarn typeorm migration:create src/database/migrations/CreateCategories
+
+// CREATE => yarn typeorm migration:create src/database/migrations/CreateCategories
+// RUN    => yarn typeorm -d ./src/database/index.ts  migration:run
+// REVERT => yarn typeorm migration:revert
 
 const dataSource = new DataSource({
   type: "postgres",
@@ -10,7 +16,7 @@ const dataSource = new DataSource({
   username: "docker",
   password: "1234",
   database: "database_rentx",
-  entities: [],
+  entities: [Category, Specification],
   migrations: ["src/database/migrations/*.ts"],
   subscribers: [],
   logging: false,
